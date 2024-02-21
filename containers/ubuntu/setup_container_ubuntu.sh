8396b633f40f543c2382377b1eb04d8042d3a1c3
timothyoei <timothyoei.dev@gmail.com>
2024-02-21 18:03:42 -0500

    Replaced vim with neovim

--- containers/ubuntu/setup_container_ubuntu.sh
+++ containers/ubuntu/setup_container_ubuntu.sh
@@ -14,25 +14,20 @@ apt-get install -y git
 git config --global user.name "Timothy Oei"
 git config --global user.email "timothyoei.dev@gmail.com"
 
-# Clone the devenv repository
+# Clone the devenvs repository
 git clone https://github.com/timothyoei/devenvs.git
 
 # Setup cli
 apt-get install -y \
-  vim \
-  tmux \
-  shutter
-
-# Install zsh
-apt-get install -y \
+  neovim \
   zsh \
   curl
-yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Customize zsh
+yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cat devenvs/common/.zshrc > ~/.zshrc
 
 # Clean up
-# rm setup_container_ubuntu.sh && rm -rf devenv
+rm setup_container_ubuntu.sh && rm -rf devenv
